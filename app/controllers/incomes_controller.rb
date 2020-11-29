d1ec40fe1d1e328b4a957187d3c567f671fdb683
TAISEI-050523 <noindex550@gmail.com>
2020-11-29 10:32:12 +0900

    READMEの修正
    income,fixed_cost,variable_costとの関係をhas_manyに改める

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,2 +1,22 @@
 class IncomesController < ApplicationController
+
+  def new
+    user = User.find(current_user.id)
+    @income = Income.new
+    @incomes = user.incomes
+  end
+
+  def create
+    @income = Income.new(income_params)
+    if @income.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def income_params
+    params.require(:income).permit(:category_id, :remarks,  :price, :date_entered).merge(user_id: current_user.id)
+  end
 end
