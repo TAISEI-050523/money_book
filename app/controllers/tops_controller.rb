8d6a2934b74318c80a9ea082b9d02d20f0a6c3fd
TAISEI-050523 <noindex550@gmail.com>
2020-12-02 18:32:49 +0900

    最近の支出内訳表示機能の実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,13 +1,15 @@
 class TopsController < ApplicationController
   def index
-    user = User.find(current_user.id)
-    @income = user.incomes.where(income_date: (1.years.ago)..(Date.today)).group('YEAR(income_date)').group('MONTH(income_date)').sum(:price)
-    @fixed_cost = user.fixed_costs.where(expense_date: (1.years.ago)..(Date.today)).group('YEAR(expense_date)').group('MONTH(expense_date)').sum(:price)
-    @variable_cost = user.variable_costs.where(expense_date: (1.years.ago)..(Date.today)).group('YEAR(expense_date)').group('MONTH(expense_date)').sum(:price)
 
-    
+    user = User.find(current_user.id)
+    one_year_period = (1.years.ago)..(Date.today)
+
+    @income = user.incomes.where(income_date: one_year_period).group('YEAR(income_date)').group('MONTH(income_date)').sum(:price)
+    @fixed_cost = user.fixed_costs.where(expense_date: one_year_period).group('YEAR(expense_date)').group('MONTH(expense_date)').sum(:price)
+    @variable_cost = user.variable_costs.where(expense_date: one_year_period).group('YEAR(expense_date)').group('MONTH(expense_date)').sum(:price)
 
     this_month = Date.today.beginning_of_month..Date.today.end_of_month                  # 期間を今月に指定
+
     @income_month = user.incomes.where(income_date: this_month).sum(:price)              # 今月の収入
     @fixed_cost_month = user.fixed_costs.where(expense_date: this_month).sum(:price)     # 今月の固定費
     @variable_month = user.variable_costs.where(expense_date: this_month).sum(:price)    # 今月の変動費
@@ -26,6 +28,9 @@ class TopsController < ApplicationController
       @sum += plan.price/(plan.purchase_date - plan.created_at.to_date).numerator
       end
     end
+    
+    @fixed_cost_category = user.fixed_costs.where(expense_date: this_month )
+    @variable_cost_category = user.variable_costs.where(expense_date: this_month )
 
   end
 end
