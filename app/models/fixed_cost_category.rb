b01e5f6944b773bfe88749ab0f5abbb7bf0e0716
TAISEI-050523 <noindex550@gmail.com>
2020-12-03 20:02:29 +0900

    トップページのフロント実装

--- app/models/fixed_cost_category.rb
+++ app/models/fixed_cost_category.rb
@@ -3,11 +3,13 @@ class FixedCostCategory < ActiveHash::Base
     { id: 1, name: '--' },
     { id: 2, name: '住居費' },
     { id: 3, name: '通信費' },
-    { id: 4, name: '水道光熱費' },
-    { id: 5, name: '教育費' },
-    { id: 6, name: '保険料' },
-    { id: 7, name: 'ローン' },
-    { id: 8, name: 'その他' }
+    { id: 4, name: '電気代' },
+    { id: 5, name: 'ガス代' },
+    { id: 6, name: '水道代' },
+    { id: 7, name: '教育費' },
+    { id: 8, name: '保険料' },
+    { id: 9, name: 'ローン' },
+    { id: 10, name: 'その他' }
   ]
   include ActiveHash::Associations
   has_many :fixed_costs
