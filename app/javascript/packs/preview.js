19f97661db87015d675ab63d2515f791fc775fd4
TAISEI-050523 <noindex550@gmail.com>
2020-12-06 16:17:48 +0900

    画像プレビュー機能の修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,6 +1,19 @@
-// if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+if (document.URL.match( /purchase_plans/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
+
+     // 表示する画像を生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+     // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
     document.getElementById('purchase_plan_image').addEventListener('change', function(e){
       // 画像が表示されている場合のみ、すでに存在している画像を削除する
       const imageContent = document.querySelector('img');
@@ -9,16 +22,7 @@
       }
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
-
-      // 画像を表示するためのdiv要素を生成
-      const imageElement = document.createElement('div');
-
-      // 表示する画像を生成
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
-      // 生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
+      createImageHTML(blob);
     });
   });
-// }
+}
