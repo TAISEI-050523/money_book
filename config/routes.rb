8c14bf7268338f9b334a0c01fe265193b5953993
TAISEI-050523 <noindex550@gmail.com>
2020-12-08 15:45:20 +0900

    収入詳細検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
-  resources :incomes, only: [:new, :create, :destroy]
+  resources :incomes, only: [:new, :create, :destroy] do
+    collection do
+      get 'search'
+    end
+  end
   resources :fixed_costs, only: [:new, :create, :destroy]
   resources :variable_costs, only: [:new, :create, :destroy]
   resources :purchase_plans, only: [:index, :new, :create, :destroy]
