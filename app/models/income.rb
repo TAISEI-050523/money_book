fcd97031ff581a6c78ab99cd9301ee8330ab49b9
TAISEI-050523 <noindex550@gmail.com>
2020-12-11 16:37:56 +0900

    フロント実装

--- app/models/income.rb
+++ app/models/income.rb
@@ -7,7 +7,6 @@ class Income < ApplicationRecord
     validates :price, numericality: { only_integer: true }
     validates :income_date
   end
-
   # 収入区分の選択が「--」の時は保存できないようにする
   validates :income_category_id, numericality: { other_than: 1, message: 'Select.' }
 
