69ebf68f84fd645327a04341949b974f98147cdd
TAISEI-050523 <noindex550@gmail.com>
2020-12-13 15:03:24 +0900

    Update incomes_spec.rb

--- spec/system/incomes_spec.rb
+++ spec/system/incomes_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "収入入力", type: :system do
       expect(page).to have_link('収 入', href: new_income_path)
       # 収入入力ページに移動する
       visit new_income_path
-      # フォームに情報を入力する
+      # 入力フォームに情報を入力する
       find("#income_income_category_id").find("option[value='#{@income_income_category_id}']").select_option
       fill_in 'income_remarks', with: @income_remarks
       fill_in 'income_price', with: @income_price
@@ -103,5 +103,53 @@ RSpec.describe '収入削除', type: :system do
       # 収入詳細欄に収入2の「削除」ボタンがないことを確認する
       expect(find("#details")).to have_no_link '削除', href: income_path(@income2)
     end
+  end
+end
+RSpec.describe "収入検索", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @income = FactoryBot.create(:income)
+    @income_income_category_id = @income.income_category_id    # 収入分類
+    @income_remarks = @income.remarks                          # 備考
+    @income_price = @income.price                              # 金額
+    @income_date = "00#{@income.income_date.year}-#{@income.income_date.mon}-#{@income.income_date.day}"  # 入金日
+  end
+  context '収入検索ができるとき' do
+    it 'ログインしたユーザーは収入入力ページで収入の検索ができる' do
+      # ログインする
+      visit new_user_session_path
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
+      # 収入入力ページへのリンクがあることを確認する
+      expect(page).to have_link('収 入', href: new_income_path)
+      # 収入入力ページに移動する
+      visit new_income_path
+      # 検索フォームに情報を入力して検索ボタンを押す
+      find("#income_income_category_id").find("option[value='#{@income_income_category_id}']").select_option
+      fill_in 'income_remarks', with: @income_remarks
+      fill_in 'income_price', with: @income_price
+      fill_in 'income_income_date', with: @income_date
+      click_on ('入 力')
+      # 検索結果ページに遷移ことを確認する
+      # expect(current_path).to eq new_income_path
+      # # 収入入力ページには先ほど入力した内容が収入詳細欄に存在することを確認する（収入分類）
+      # expect(find("#details")).to have_content(@income.income_category.name)
+      # # 収入入力ページには先ほど入力した内容が収入詳細欄に存在することを確認する（備考）
+      # expect(find("#details")).to have_content(@income_remarks)
+      # # 収入入力ページには先ほど入力した内容が収入詳細欄に存在することを確認する（金額）
+      # expect(find("#details")).to have_content(@income_price.to_s(:delimited))
+      # # 収入入力ページには先ほど入力した内容が収入詳細欄に存在することを確認する（入金日）
+      # expect(find("#details")).to have_content(@income_income_date)
+    end
+    # context '収入入力ができないとき'do
+    #   it 'ログインしていないと収入入力ページに遷移できない' do
+    #     # 収入入力ページに遷移する
+    #     visit new_income_path
+    #     # ログインページにリダイレクトされる
+    #     expect(current_path).to eq new_user_session_path
+    #   end
+    # end
   end
 end
